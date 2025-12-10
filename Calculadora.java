3f5c81e9897569b0d73c22b3af6d3d1140daac6c
Bintins <guerrerogonzalesk@gmail.com>
2025-12-09 21:02:54 -0500

    2do_cambio_calcul

--- Calculadora.java
+++ Calculadora.java
@@ -1,9 +1,9 @@
 public class Calculadora {
     public int sumar(int a, int b){
-        return a + b;
+        return a + b + 1;
     }
 
     public int restar(int a, int b){
-        return a - b;
+        return a - b - 1;
     }
 }
